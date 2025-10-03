8f09a56f1906ae9c40206072d9510bd798b93ba9
Cha-Khiao <p.prasopphol203@gmail.com>
2025-10-03 14:12:51 +0700

    update hairstyle

--- lib/models/hairstyle_model.dart
+++ lib/models/hairstyle_model.dart
@@ -5,8 +5,8 @@ class Hairstyle {
   final List<String> imageUrls;
   final String overlayImageUrl;
   final String gender;
-  final List<String> tags; // <-- เพิ่มเข้ามา
-  final List<String> suitableFaceShapes; // <-- เพิ่มเข้ามา
+  final List<String> tags;
+  final List<String> suitableFaceShapes;
   final int numReviews;
   final double averageRating;
 
@@ -17,8 +17,8 @@ class Hairstyle {
     required this.imageUrls,
     required this.overlayImageUrl,
     required this.gender,
-    required this.tags, // <-- เพิ่มเข้ามา
-    required this.suitableFaceShapes, // <-- เพิ่มเข้ามา
+    required this.tags,
+    required this.suitableFaceShapes,
     required this.numReviews,
     required this.averageRating,
   });
@@ -33,13 +33,53 @@ class Hairstyle {
           : [],
       overlayImageUrl: json['overlayImageUrl'] ?? '',
       gender: json['gender'] ?? 'Unisex',
-      // แปลงค่าและป้องกัน null
       tags: json['tags'] != null ? List<String>.from(json['tags']) : [],
       suitableFaceShapes: json['suitableFaceShapes'] != null
           ? List<String>.from(json['suitableFaceShapes'])
           : [],
       numReviews: json['numReviews'] ?? 0,
       averageRating: (json['averageRating'] ?? 0).toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      '_id': id,
+      'name': name,
+      'description': description,
+      'imageUrls': imageUrls,
+      'overlayImageUrl': overlayImageUrl,
+      'gender': gender,
+      'tags': tags,
+      'suitableFaceShapes': suitableFaceShapes,
+      'numReviews': numReviews,
+      'averageRating': averageRating,
+    };
+  }
+
+  Hairstyle copyWith({
+    String? id,
+    String? name,
+    String? description,
+    List<String>? imageUrls,
+    String? overlayImageUrl,
+    String? gender,
+    List<String>? tags,
+    List<String>? suitableFaceShapes,
+    int? numReviews,
+    double? averageRating,
+  }) {
+    return Hairstyle(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      description: description ?? this.description,
+      imageUrls: imageUrls ?? this.imageUrls,
+      overlayImageUrl: overlayImageUrl ?? this.overlayImageUrl,
+      gender: gender ?? this.gender,
+      tags: tags ?? this.tags,
+      suitableFaceShapes: suitableFaceShapes ?? this.suitableFaceShapes,
+      numReviews: numReviews ?? this.numReviews,
+      averageRating: averageRating ?? this.averageRating,
     );
   }
 }
